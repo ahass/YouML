1f20cd3928f8e7c856f8e7a628d7f574271b9a65
André Hass <ah@codanargus.com>
2022-09-14 12:32:38 +0200

    Add project

---
+++ YouML/Models/ClassModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouML.Models
+{
+    public class ClassModel
+    {
+        public NameSyntax Namespace { get; set; }
+
+        public ClassDeclarationSyntax ClassDeclarationSyntax { get; set; }
+
+        public IEnumerable<MethodDeclarationSyntax> MethodDeclarationSyntax { get; set; }
+
+        public IEnumerable<PropertyDeclarationSyntax> PropertyDeclarationSyntax { get; set; }
+
+        public IEnumerable<FieldDeclarationSyntax> FieldDeclarationSyntax { get; set; }
+        
+        public IEnumerable<ConstructorDeclarationSyntax> ConstructorDeclarationSyntax { get; set; }
+        
+        public IEnumerable<EventFieldDeclarationSyntax> EventDeclarationSyntax { get; set; }
+
+        public IEnumerable<EnumDeclarationSyntax> EnumDeclarationSyntax { get; set; }
+
+    }
+}
